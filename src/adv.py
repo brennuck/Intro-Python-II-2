090867665e80d5292e5200c4cf992510b88513fa
Brennon Nuckols <brennuck@gmail.com>
2020-04-29 10:14:35 -0600

    items rendering

--- src/adv.py
+++ src/adv.py
@@ -1,26 +1,25 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty passages run north and east."""),
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
 }
+
+sword = Item("Sword", "Dull but will do the trick")
+fork = Item("Fork", "You imagine all the food you could be eating")
+
+room['outside'].items = []
+room['foyer'].items = []
+room['overlook'].items = [sword]
+room['narrow'].items = [fork]
+room['treasure'].items = []
 
 
 # Link rooms together
@@ -54,23 +53,23 @@ player1 = Player("Brennon", room['outside'])
 # If the user enters "q", quit the game.
 
 def move(player):
-    print(player.current_room)
+    print(f"\nCurrent room:{player.current_room}, \nItems in the room: {player.current_room.items}")
     direction_choice = input("\n Where would you like to go? n, e, s, w, or q to quit \n")
 
     if direction_choice == "n":
-        print(player.current_room)
+        print(f"\nCurrent room:{player.current_room}, \nItems in the room: {player.current_room.items}")
         player.try_north()
         move(player)
     elif direction_choice == "e":
-        print(player.current_room)
+        print(f"\nCurrent room:{player.current_room}, \nItems in the room: {player.current_room.items}")
         player.try_east()
         move(player)
     elif direction_choice == "s":
-        print(player.current_room)
+        print(f"\nCurrent room:{player.current_room}, \nItems in the room: {player.current_room.items}")
         player.try_south()
         move(player)
     elif direction_choice == "w":
-        print(player.current_room)
+        print(f"\nCurrent room:{player.current_room}, \nItems in the room: {player.current_room.items}")
         player.try_west()
         move(player)
     elif direction_choice == "q":
--- src/room.py
+++ src/room.py
@@ -1,11 +1,14 @@
+from item import Item
+
 class Room:
-    def __init__(self, name, description, n_to=None, e_to=None, s_to=None, w_to=None):
+    def __init__(self, name, description, n_to=None, e_to=None, s_to=None, w_to=None, list=Item):
         self.name = name
         self.description = description
         self.n_to = n_to
         self.e_to = e_to
         self.s_to = s_to
         self.w_to = w_to
+        self.list = list
 
     def __str__(self):
         return f"{self.name}, {self.description}"
